005b18aca8495f839e4901acab2dbbecb074cde9
lecielpourlamer <shlim20190902@gmail.com>
2023-10-20 17:38:01 +0900

    inflearn querydsl

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,38 @@
+package study.querydsl.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")   // 외래키(FK) 컬럼 이름
+    private Team team;
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+}
