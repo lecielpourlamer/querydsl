95ddc2a594df5efe5fa4f0f8a2d1a97a73422692
lecielpourlamer <shlim20190902@gmail.com>
2023-10-20 18:07:48 +0900

    inflearn 2장 완료

--- src/main/java/study/querydsl/entity/Member.java
+++ src/main/java/study/querydsl/entity/Member.java
@@ -1,13 +1,14 @@
 package study.querydsl.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
@@ -21,6 +22,14 @@ public class Member {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id")   // 외래키(FK) 컬럼 이름
     private Team team;
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
 
     public Member(String username, int age, Team team) {
         this.username = username;
